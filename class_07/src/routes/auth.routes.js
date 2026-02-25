d1659c0ab0410d2175817290b2a7265711b43851
Arush Kumar <arushkumarbjn123@gmail.com>
2026-02-25 09:42:25 +0530

    add login api

--- class_07/src/routes/auth.routes.js
+++ class_07/src/routes/auth.routes.js
@@ -39,4 +39,44 @@ authRouter.post("/register", async (req, res) => {
   });
 });
 
+
+/*
+  - POST /api/auth/login
+*/
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "User not found",
+    });
+  }
+
+  const isPasswordMatched = user.password === password;
+
+  if (!isPasswordMatched) {
+    return res.status(400).json({
+      message: "Invalid password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET
+  )
+
+  res.cookie("jwt_token", token)
+
+  res.status(200).json({
+    message: "User login successfully",
+    user,
+    token
+  });
+});
+
 export default authRouter;
